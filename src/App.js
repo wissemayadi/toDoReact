22d1f097141ca70ed41c2bec40a0e4ab76b168ab
wissemayadi <wissem-ayadi@hotmail.com>
2021-10-05 16:25:01 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import {useState,useEffect}  from 'react'
+import {BrowserRouter as Router , Route} from 'react-router-dom'
 import './App.css';
 import Header from './components/header'
+import Footer from './components/footer';
+import About from './components/about';
 import Tasks from './components/tasks'
 import AddTask  from './components/addTask';
 function App() {
@@ -15,22 +18,24 @@ function App() {
  }
   getTasks()
   },[])
-//fetch data
+//fetch tasks
 const fetchTasks=async()=>{
   const res=await fetch(' http://localhost:5000/tasks');
   const data= await res.json()
   console.log(data)
  return data
 }
+// fecth one task
+const fetchTask=async(id)=>{
+  const res=await fetch(`http://localhost:5000/tasks/${id}` );
+  const data= await res.json()
+  console.log(data)
+ return data
+}
+
 
 //add task
 
-// const addTask=(task)=>{ 
-// const id =Math.floor(Math.random() * 10000) +1
-// console.log(id)
-
-// const newTask={id,...task}
-// setTasks([...tasks,newTask])
 const addTask=async(task)=>{
 const res= await fetch('http://localhost:5000/tasks',{
   method:'POST',
@@ -60,22 +65,47 @@ const deleteTask = async (id) => {
     ? setTasks(tasks.filter((task) => task.id !== id))
     : alert('Error Deleting This Task')
 }
-//toogle reminder
-const toogleReminder=(id)=>{
-setTasks(tasks.map((task)=>task.id===id ? {...task,reminder:!task.reminder}: task)
-)
 
+const toogleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(updTask),
+  })
+
+  const data = await res.json()
+
+  setTasks(
+    tasks.map((task) =>
+      task.id === id ? { ...task, reminder: data.reminder } : task
+    )
+  )
 }
 
   return (
+    <Router>
     <div className='container'>
       <Header onAdd={()=>setShowAddTask(!showAddTask)}
       showAdd={showAddTask} />
+
+      
+      <Route path='/' exact render={(props)=>
+      <>   
       {showAddTask &&  <AddTask onAdd={addTask}  />}
      {tasks.length > 0 ? (<Tasks tasks={tasks} 
      onDelete={deleteTask} 
      onToogle={toogleReminder} />) :( 'No task to show')} 
+      </>
+      }/>
+     <Route path='/about' component={About}/>
+   <Footer/>
     </div>
+    </Router>
   );
 }
 
